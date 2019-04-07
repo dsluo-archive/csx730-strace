e11deeed2ad75e552ede8397fe1bcad2bfea1cdd
dsluo <mail@dsluo.me>
2019-04-07 17:40:34 -0400

    strace

--- main.c
+++ main.c
@@ -1,11 +1,21 @@
 #include <unistd.h>
 #include <stdio.h>
+#include <stdlib.h>
 
 int main() {
-  char buffer [3] = " \n";
-  buffer[0] = 0x30;
-  ssize_t n = write(1, buffer, 2);
-  if (n < 0) perror("main");
-  return 0;
+    char buffer [3] = " \n";
+
+
+    for (int i = 48; i < 58; i++) {
+        buffer[0] = i;
+        size_t n = write(1, buffer, 2);
+        if (n < 0) perror("main");
+
+        
+    }
+
+    void * ptr = malloc(131049);
+
+    return 0;
 } // main
 
